9317670b5a022535dc993bedc3a85d9bb295c627
solairerove <hax2033@gmail.com>
2016-07-04 18:44:54 +0300

    TODO if have report pass as last param

--- src/test/jmeter/run.sh
+++ src/test/jmeter/run.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-for i in "$@"
+#for i in "$@"
+#do
+#    echo Argument: $i
+#    $JMETER_HOME/bin/jmeter.sh -n -t $i
+#done
+#TODO if have report pass as last param
+jtl=".jtl";
+arr="$@";
+
+for i in "${arr[@]}"
 do
-    echo Argument: $i
-    $JMETER_HOME/bin/jmeter.sh -n -t $i
+    if [[ ${i} == *"${jtl}"* ]]; then
+        echo "it's there!";
+    else
+        echo "fuck no"
+    fi
+#   echo Argument: ${i}
+#   $JMETER_HOME/bin/jmeter.sh -n -t ${i}
 done
+
 
 #$JMETER_HOME/bin/jmeter.sh -n -t $1 -l $2
