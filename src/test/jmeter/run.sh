9467c4268052ecdec2481d52cdf415119452feaa
solairerove <hax2033@gmail.com>
2016-07-04 18:04:34 +0300

    pass array of arguments to script

--- src/test/jmeter/run.sh
+++ src/test/jmeter/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-$JMETER_HOME/bin/jmeter.sh -n -t $1 -l $2
+for i in "$@"
+do
+    echo Argument: $i
+    $JMETER_HOME/bin/jmeter.sh -n -t $i
+done
+
+#$JMETER_HOME/bin/jmeter.sh -n -t $1 -l $2
